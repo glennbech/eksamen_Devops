1b1a0cdfaecc9225c75b3a2297ac21337cc42efd
hagenh <haakonhagen@live.no>
2021-11-25 12:42:00 +0100

    Added tests

---
+++ src/test/java/com/pgr301/exam/ReallyShakyBankingCoreSystemServiceTest.java
@@ -0,0 +1,36 @@
+package com.pgr301.exam;
+
+import com.pgr301.exam.model.Account;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReallyShakyBankingCoreSystemServiceTest {
+
+    @Test
+    void transfer() {
+    }
+
+    @Test
+    void updateAccount() {
+        ReallyShakyBankingCoreSystemService shaky = new ReallyShakyBankingCoreSystemService();
+        Account test = shaky.getAccount("5401");
+        test.setCurrency("USD");
+        shaky.updateAccount(test);
+    }
+
+    @Test
+    void balance() {
+        ReallyShakyBankingCoreSystemService shaky = new ReallyShakyBankingCoreSystemService();
+        Account test = shaky.getAccount("1234");
+        BigDecimal testBalance = new BigDecimal(1000000);
+        test.setBalance(testBalance);
+        assertEquals(testBalance, test.getBalance());
+    }
+
+    @Test
+    void getAccount() {
+    }
+}
